4642d45f8f5cba9182c80c9367f471a931fdfa75
hblado <hblado@protonmail.ch>
2024-08-28 14:11:51 -0300

    UI changes

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,70 +1,88 @@
-'use client';
+"use client";
 
-import { useForm } from 'react-hook-form';
+import { useForm } from "react-hook-form";
 import { Roboto_Slab } from "next/font/google";
-import * as z from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { signIn } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import style from "./login.module.css";
 
-const robotoSlab = Roboto_Slab({ weight: ['400'], subsets: ['latin'] });
+const robotoSlab = Roboto_Slab({ weight: ["400"], subsets: ["latin"] });
 
 const FormSchema = z.object({
-  email: z.string().min(1, 'Email is required').email('Invalid email'),
-  password: z.string().min(1, 'Password is required')
+  email: z.string().min(1, "Email is required").email("Invalid email"),
+  password: z.string().min(1, "Password is required"),
 });
 
 const LoginForm = () => {
   const router = useRouter();
-  const { register, handleSubmit, formState: { errors } } = useForm<z.infer<typeof FormSchema>>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
-      email: '',
-      password: ''
-    }
+      email: "",
+      password: "",
+    },
   });
 
   const onSubmit = async (values: z.infer<typeof FormSchema>) => {
-    const signInData = await signIn('credentials', {
+    const signInData = await signIn("credentials", {
       email: values.email,
       password: values.password,
-      redirect: false
+      redirect: false,
     });
 
     if (signInData?.error) {
     } else {
-      router.push('/dashboard');
+      router.push("/dashboard");
     }
   };
 
   return (
-    <div className={`${robotoSlab.className} inline-flex flex-col justify-self-center`}>
-      <h1 className="text-3xl text-white truncate">Login</h1>
-      <div className="inline-flex flex-col text-xl">
-        <form onSubmit={handleSubmit(onSubmit)} method="POST">
-          <div className="mb-6">
-            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-            <input 
-              type="email"
-              {...register('email')}
-              className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${errors.email ? 'border-red-500' : ''}`}
-            />
-            {errors.email && <p className="text-red-500 text-xs mt-1">{errors.email.message}</p>}
-          </div>
-          <div className="mb-6">
-            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-            <input
-              type="password"
-              {...register('password')}
-              className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${errors.password ? 'border-red-500' : ''}`}
-            />
-            {errors.password && <p className="text-red-500 text-xs mt-1">{errors.password.message}</p>}
-          </div>
-          <button type="submit" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:focus:ring-green-800">
-            Submit
-          </button>
-        </form>
-      </div>
+    <div
+      className={`${robotoSlab.className} inline-flex flex-col justify-self-center`}
+    >
+      <h1 className={style.title}>Login</h1>
+      <form
+        className={style.formSection}
+        onSubmit={handleSubmit(onSubmit)}
+        method="POST"
+      >
+        <div className={style.formComponent}>
+          <label className={style.formLabel}>Email</label>
+          <input
+            type="email"
+            {...register("email")}
+            className={style.formInput}
+          />
+          {errors.email && (
+            <p className="text-red-500 text-xs mt-1">{errors.email.message}</p>
+          )}
+        </div>
+        <div className={style.formComponent}>
+          <label className={style.formLabel}>Password</label>
+          <input
+            type="password"
+            {...register("password")}
+            className={style.formInput}
+          />
+          {errors.password && (
+            <p className="text-red-500 text-xs mt-1">
+              {errors.password.message}
+            </p>
+          )}
+        </div>
+        <button
+          type="submit"
+          className={style.confirmButton}
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -2,6 +2,7 @@
 import { Roboto_Slab } from "next/font/google";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import style from './signup.module.css'
 const robotoSlab = Roboto_Slab({ weight: ["400"], subsets: ["latin"] });
 
 export default function Home() {
@@ -36,18 +37,15 @@ export default function Home() {
     })
       .then((response) => {
         if (!response.ok) {
-          // Lida com respostas de erro (status code fora da faixa 200-299)
           return response.json().then((errorData) => {
             console.error("Error:", errorData);
             alert(`Error: ${errorData.message}`);
-            throw new Error(errorData.message); // Opcional: para quebrar a cadeia de promessas
+            throw new Error(errorData.message);
           });
         }
-        return response.json(); // Continue se a resposta for bem-sucedida
+        return response.json();
       })
       .then((data) => {
-        // Aqui lidamos com a resposta de sucesso
-        console.log("Success:", data);
         alert(`Success: ${data.message}`);
         setFormData({
           name: "",
@@ -55,7 +53,6 @@ export default function Home() {
           password: "",
           passwordConfirm: "",
         });
-        // Navegar para outra página, se necessário
         router.push("/api/auth/signin");
       })
       .catch((error) => {
@@ -66,25 +63,25 @@ export default function Home() {
 
   return (
     <div
-      className={`${robotoSlab.className} inline-flex flex-col justify-self-center`}
+      className={`${robotoSlab.className}`}
     >
-      <h1 className="text-3xl text-white truncate">Sign Up</h1>
-      <div className="inline-flex flex-col text-xl">
-        <form method="POST" action={`/api/user`} onSubmit={submitForm}>
-          <div className="mb-6 mt-14">
-            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-              Name
+      <h1 className={style.title}>Sign Up</h1>
+
+        <form className={style.formSection} method="POST" action={`/api/user`} onSubmit={submitForm}>
+          <div className={style.formComponent} >
+            <label className={style.formLabel}>
+              Full Name
             </label>
             <input
               type="text"
               name="name"
               value={formData.name}
               onChange={handleInput}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blu-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className={style.formInput}
             />
           </div>
-          <div className="mb-6">
-            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          <div className={style.formComponent}>
+            <label className={style.formLabel}>
               Email
             </label>
             <input
@@ -92,11 +89,11 @@ export default function Home() {
               name="email"
               value={formData.email}
               onChange={handleInput}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blu-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className={style.formInput}
             />
           </div>
-          <div className="mb-6">
-            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          <div className={style.formComponent}>
+            <label className={style.formLabel}>
               Password
             </label>
             <input
@@ -104,11 +101,11 @@ export default function Home() {
               name="password"
               value={formData.password}
               onChange={handleInput}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blu-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className={style.formInput}
             />
           </div>
-          <div className="mb-6">
-            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          <div className={style.formComponent}>
+            <label className={style.formLabel}>
               Confirm Password
             </label>
             <input
@@ -116,17 +113,16 @@ export default function Home() {
               name="passwordConfirm"
               value={formData.passwordConfirm}
               onChange={handleInput}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blu-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className={style.formInput}
             />
           </div>
           <button
             type="submit"
-            className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:focus:ring-green-800"
+            className={style.confirmButton}
           >
             Submit
           </button>
         </form>
-      </div>
     </div>
   );
 }
